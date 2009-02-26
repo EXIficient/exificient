3280e114c9d94ad2514e7c5dfa51b7051d9e263f
daniel_peintner <daniel_peintner@550fd6a5-4035-4c25-860b-87adce24ffb0>
2009-02-26 11:33:23 +0000

    changes copyright date range

--- trunk/src/main/com/siemens/ct/exi/grammar/rule/RuleDocEnd.java
+++ trunk/src/main/com/siemens/ct/exi/grammar/rule/RuleDocEnd.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007, 2008 Siemens AG
+ * Copyright (C) 2007-2009 Siemens AG
  *
  * This program and its interfaces are free software;
  * you can redistribute it and/or modify
