02dc3258281a7580dec9b53639e66b0244ce49bb
daniel_peintner <danielpeintner@users.noreply.github.com>
2009-02-27 14:33:28 +0000

    . revised SAXDecoder constructor of . *softens* visibility of methods

--- src/main/com/siemens/ct/exi/helpers/DefaultEXIFactory.java
+++ src/main/com/siemens/ct/exi/helpers/DefaultEXIFactory.java
@@ -182,7 +182,7 @@ public class DefaultEXIFactory implements EXIFactory {
 	}
 
 	public XMLReader createEXIReader() {
-		return new SAXDecoder(this, exiBodyOnly);
+		return new SAXDecoder(this);
 	}
 
 	public TypeEncoder createTypeEncoder() {
