053c2cca00349bc2632e9c7eb996b70f2bb05078
daniel_peintner <daniel_peintner@550fd6a5-4035-4c25-860b-87adce24ffb0>
2010-08-24 11:16:36 +0000

    * integrate XML schema for EXI Options document without external resource

--- trunk/src/main/com/siemens/ct/exi/core/AbstractEXIHeader.java
+++ trunk/src/main/com/siemens/ct/exi/core/AbstractEXIHeader.java
@@ -18,6 +18,7 @@
 
 package com.siemens.ct.exi.core;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import com.siemens.ct.exi.EXIFactory;
@@ -37,9 +38,6 @@ import com.siemens.ct.exi.helpers.DefaultEXIFactory;
  */
 
 public abstract class AbstractEXIHeader {
-
-	/* system resource for EXI Options XML schema document */
-	public static final String RESOURCE_EXI_OPTIONS_XSD = "EXIOptionsHeader.xsd";
 
 	public static final String HEADER = "header";
 	public static final String LESSCOMMON = "lesscommon";
@@ -75,9 +73,8 @@ public abstract class AbstractEXIHeader {
 
 	protected EXIFactory getHeaderFactory() throws EXIException {
 		if (headerFactory == null) {
-			InputStream isXSD = ClassLoader
-					.getSystemResourceAsStream(RESOURCE_EXI_OPTIONS_XSD);
-			Grammar headerGrammar = grammarFactory.createGrammar(isXSD);
+			InputStream is = new ByteArrayInputStream(EXI_OPTIONS_XSD.getBytes());
+			Grammar headerGrammar = grammarFactory.createGrammar(is);
 
 			headerFactory = DefaultEXIFactory.newInstance();
 			headerFactory.setGrammar(headerGrammar);
@@ -86,5 +83,7 @@ public abstract class AbstractEXIHeader {
 
 		return headerFactory;
 	}
+
+	static final String EXI_OPTIONS_XSD = "<xsd:schema targetNamespace='http://www.w3.org/2009/exi' xmlns:xsd='http://www.w3.org/2001/XMLSchema' elementFormDefault='qualified'><xsd:element name='header'><xsd:complexType><xsd:sequence><xsd:element name='lesscommon' minOccurs='0'><xsd:complexType><xsd:sequence><xsd:element name='uncommon' minOccurs='0'><xsd:complexType><xsd:sequence><xsd:any namespace='##other' minOccurs='0' maxOccurs='unbounded' processContents='skip' /><xsd:element name='alignment' minOccurs='0'><xsd:complexType><xsd:choice><xsd:element name='byte'><xsd:complexType /></xsd:element><xsd:element name='pre-compress'><xsd:complexType /></xsd:element></xsd:choice></xsd:complexType></xsd:element><xsd:element name='selfContained' minOccurs='0'><xsd:complexType /></xsd:element><xsd:element name='valueMaxLength' minOccurs='0'><xsd:simpleType><xsd:restriction base='xsd:unsignedInt' /></xsd:simpleType></xsd:element><xsd:element name='valuePartitionCapacity' minOccurs='0'><xsd:simpleType><xsd:restriction base='xsd:unsignedInt' /></xsd:simpleType></xsd:element><xsd:element name='datatypeRepresentationMap' minOccurs='0' maxOccurs='unbounded'><xsd:complexType><xsd:sequence><xsd:any namespace='##other' processContents='skip' /><xsd:any processContents='skip' /></xsd:sequence></xsd:complexType></xsd:element></xsd:sequence></xsd:complexType></xsd:element><xsd:element name='preserve' minOccurs='0'><xsd:complexType><xsd:sequence><xsd:element name='dtd' minOccurs='0'><xsd:complexType /></xsd:element><xsd:element name='prefixes' minOccurs='0'><xsd:complexType /></xsd:element><xsd:element name='lexicalValues' minOccurs='0'><xsd:complexType /></xsd:element><xsd:element name='comments' minOccurs='0'><xsd:complexType /></xsd:element><xsd:element name='pis' minOccurs='0'><xsd:complexType /></xsd:element></xsd:sequence></xsd:complexType></xsd:element><xsd:element name='blockSize' minOccurs='0'><xsd:simpleType><xsd:restriction base='xsd:unsignedInt' /></xsd:simpleType></xsd:element></xsd:sequence></xsd:complexType></xsd:element><xsd:element name='common' minOccurs='0'><xsd:complexType><xsd:sequence><xsd:element name='compression' minOccurs='0'><xsd:complexType /></xsd:element><xsd:element name='fragment' minOccurs='0'><xsd:complexType /></xsd:element><xsd:element name='schemaId' minOccurs='0' nillable='true'><xsd:simpleType><xsd:restriction base='xsd:string' /></xsd:simpleType></xsd:element></xsd:sequence></xsd:complexType></xsd:element><xsd:element name='strict' minOccurs='0'><xsd:complexType /></xsd:element></xsd:sequence></xsd:complexType></xsd:element><xsd:simpleType name='base64Binary'><xsd:restriction base='xsd:base64Binary'/></xsd:simpleType><xsd:simpleType name='hexBinary' ><xsd:restriction base='xsd:hexBinary'/></xsd:simpleType><xsd:simpleType name='boolean' ><xsd:restriction base='xsd:boolean'/></xsd:simpleType><xsd:simpleType name='decimal' ><xsd:restriction base='xsd:decimal'/></xsd:simpleType><xsd:simpleType name='double' ><xsd:restriction base='xsd:double'/></xsd:simpleType><xsd:simpleType name='integer' ><xsd:restriction base='xsd:integer'/></xsd:simpleType><xsd:simpleType name='string' ><xsd:restriction base='xsd:string'/></xsd:simpleType><xsd:simpleType name='dateTime' ><xsd:restriction base='xsd:dateTime'/></xsd:simpleType><xsd:simpleType name='date' ><xsd:restriction base='xsd:date'/></xsd:simpleType><xsd:simpleType name='time' ><xsd:restriction base='xsd:time'/></xsd:simpleType><xsd:simpleType name='gYearMonth' ><xsd:restriction base='xsd:gYearMonth'/></xsd:simpleType><xsd:simpleType name='gMonthDay' ><xsd:restriction base='xsd:gMonthDay'/></xsd:simpleType><xsd:simpleType name='gYear' ><xsd:restriction base='xsd:gYear'/></xsd:simpleType><xsd:simpleType name='gMonth' ><xsd:restriction base='xsd:gMonth'/></xsd:simpleType><xsd:simpleType name='gDay' ><xsd:restriction base='xsd:gDay'/></xsd:simpleType><xsd:simpleType name='ieeeBinary32' ><xsd:restriction base='xsd:float'/></xsd:simpleType><xsd:simpleType name='ieeeBinary64' ><xsd:restriction base='xsd:double'/></xsd:simpleType></xsd:schema>";
 
 }
