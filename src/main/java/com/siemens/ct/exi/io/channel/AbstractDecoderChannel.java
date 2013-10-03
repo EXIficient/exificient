4f2de0641b59256d220b81f34a776e379e626727
daniel_peintner <danielpeintner@users.noreply.github.com>
2013-10-03 12:06:35 +0000

    Re-use boolean values on decoder side

--- src/main/java/com/siemens/ct/exi/io/channel/AbstractDecoderChannel.java
+++ src/main/java/com/siemens/ct/exi/io/channel/AbstractDecoderChannel.java
@@ -43,15 +43,20 @@ public abstract class AbstractDecoderChannel implements DecoderChannel {
 	/* long == 64 bits, 9 * 7bits = 63 bits */
 	private final static int MAX_OCTETS_FOR_LONG = 9;
 	
+	/* boolean values */
+	private final BooleanValue bvTrue = new BooleanValue(true);
+	private final BooleanValue bvFalse = new BooleanValue(false);
+	
 	/* Helper for building strings */
 	protected StringBuilder sbHelper;
 
 	
 	public AbstractDecoderChannel() {
 	}
-
+	
 	public BooleanValue decodeBooleanValue() throws IOException {
-		return new BooleanValue(decodeBoolean());
+		// return new BooleanValue(decodeBoolean());
+		return decodeBoolean() ? bvTrue : bvFalse;
 	}
 
 	/**
