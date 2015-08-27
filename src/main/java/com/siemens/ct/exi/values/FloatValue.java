463313b80dca0c5f62587e50b9c11499a8f90280
daniel_peintner <danielpeintner@users.noreply.github.com>
2015-08-27 09:16:52 +0000

    * implements float normalization http://www.w3.org/TR/exi-c14n/#dt-float * fixes overflow bug in float equals()

--- src/main/java/com/siemens/ct/exi/values/FloatValue.java
+++ src/main/java/com/siemens/ct/exi/values/FloatValue.java
@@ -23,9 +23,9 @@ import java.io.IOException;
 import com.siemens.ct.exi.Constants;
 
 /**
- * A Float represented as two consecutive Integers. The first Integer
- * represents the mantissa of the floating point number and the second
- * Integer represents the 10-based exponent of the floating point number
+ * A Float represented as two consecutive Integers. The first Integer represents
+ * the mantissa of the floating point number and the second Integer represents
+ * the 10-based exponent of the floating point number
  * 
  * @author Daniel.Peintner.EXT@siemens.com
  * @author Joerg.Heuer@siemens.com
@@ -36,46 +36,80 @@ import com.siemens.ct.exi.Constants;
 public class FloatValue extends AbstractValue {
 
 	private static final long serialVersionUID = 5799093635881195073L;
-	
+
 	protected final IntegerValue mantissa;
 	protected final IntegerValue exponent;
 
 	protected int slenMantissa = -1;
 
-	public static IntegerValue FLOAT_SPECIAL_VALUES = IntegerValue.valueOf(Constants.FLOAT_SPECIAL_VALUES);
-	public static IntegerValue FLOAT_NEGATIVE_INFINITY = IntegerValue.valueOf(-1);
-	public static IntegerValue FLOAT_POSITIVE_INFINITY = IntegerValue.valueOf(1);
-	public static IntegerValue FLOAT_NaN = IntegerValue.valueOf(0);
-	
-	
+	public static IntegerValue FLOAT_SPECIAL_VALUES = IntegerValue
+			.valueOf(Constants.FLOAT_SPECIAL_VALUES);
+	public static IntegerValue FLOAT_NEGATIVE_INFINITY = IntegerValue
+			.valueOf(-1);
+	public static IntegerValue FLOAT_POSITIVE_INFINITY = IntegerValue
+			.valueOf(1);
+	public static IntegerValue FLOAT_NaN = IntegerValue.ZERO;
+
 	protected Double f;
 
 	public FloatValue(IntegerValue mantissa, IntegerValue exponent) {
 		super(ValueType.FLOAT);
+
+		// http://www.w3.org/TR/exi-c14n/#dt-float
+		if (IntegerValue.ZERO.equals(mantissa)) {
+			// If the mantissa is 0 and the exponent value is not
+			// -(2<sup>14</sup>) to indicate one of the
+			// special values then the exponent MUST be 0
+			if (!FLOAT_SPECIAL_VALUES.equals(exponent)) {
+				exponent = IntegerValue.ZERO;
+			}
+		} else {
+			// If the mantissa is not 0, mantissas MUST have no trailing zeros
+			// e.g., 12300E0 --> 123E2
+			long lm = mantissa.longValue();
+			long le = exponent.longValue();
+			boolean modified = false;
+			while (lm % 10 == 0) {
+				// multiple of 10
+				lm /= 10;
+				le++;
+				modified = true;
+			}
+			if (modified) {
+				mantissa = IntegerValue.valueOf(lm);
+				exponent = IntegerValue.valueOf(le);
+			}
+		}
+		// If the exponent value is -(2^14) and the mantissa value is neither 1
+		// nor -1, to indicate the special value not-a-number (NaN), the
+		// mantissa MUST be 0.
+		if (FLOAT_SPECIAL_VALUES.equals(exponent)
+				&& !FLOAT_NEGATIVE_INFINITY.equals(mantissa)
+				&& !FLOAT_POSITIVE_INFINITY.equals(mantissa)) {
+			mantissa = FLOAT_NaN; // 0
+		}
+
 		this.mantissa = mantissa;
 		this.exponent = exponent;
 	}
-	
+
 	public FloatValue(long mantissa, long exponent) {
-		super(ValueType.FLOAT);
-		this.mantissa = IntegerValue.valueOf(mantissa);
-		this.exponent = IntegerValue.valueOf(exponent);
+		this(IntegerValue.valueOf(mantissa), IntegerValue.valueOf(exponent));
 	}
-	
+
 	/**
 	 * Integer represents the mantissa of the floating point number
 	 */
 	public IntegerValue getMantissa() {
 		return mantissa;
 	}
-	
+
 	/**
 	 * Integer represents the 10-based exponent of the floating point number
 	 */
 	public IntegerValue getExponent() {
 		return exponent;
 	}
-	
 
 	public static FloatValue parse(String value) {
 		try {
@@ -99,30 +133,30 @@ public class FloatValue extends AbstractValue {
 					if (indexE == -1) {
 						indexE = value.indexOf('e');
 					}
-					
+
 					char c;
 					char[] chars = value.toCharArray();
-					
+
 					// status: detecting sign
 					boolean negative = ((c = chars[0]) == '-') ? true : false;
-					
+
 					int lenMantissa = (indexE == -1) ? chars.length : indexE;
 					int startMantissa = negative || c == '+' ? 1 : 0;
-					
+
 					boolean decPoint = false;
 					int decimalDigits = 0;
 					sMantissa = 0;
 					sExponent = 0;
-					
+
 					// invalid floats
 					if (lenMantissa == 0) {
 						return null;
 					}
-					
+
 					// parsing mantissa
-					for(int i = startMantissa; i<lenMantissa; i++) {
+					for (int i = startMantissa; i < lenMantissa; i++) {
 						c = chars[i];
-						switch(c) {
+						switch (c) {
 						case '0':
 						case '1':
 						case '2':
@@ -149,7 +183,7 @@ public class FloatValue extends AbstractValue {
 							return null;
 						}
 					}
-					
+
 					// mantissa overflow ?
 					if (sMantissa < 0) {
 						if (negative) {
@@ -160,18 +194,17 @@ public class FloatValue extends AbstractValue {
 							return null;
 						}
 					}
-					
+
 					if (negative) {
 						sMantissa = (-1) * sMantissa;
 					}
-					
-					
+
 					// parsing exponent
 					boolean negativeExp = false;
 					if (indexE != -1) {
-						for(int i=indexE+1; i<chars.length; i++) {
+						for (int i = indexE + 1; i < chars.length; i++) {
 							c = chars[i];
-							switch(c) {
+							switch (c) {
 							case '0':
 								sExponent = 10 * sExponent;
 								break;
@@ -200,11 +233,10 @@ public class FloatValue extends AbstractValue {
 							}
 						}
 					}
-					
-					sExponent = negativeExp ? (-1)*sExponent : sExponent;
+
+					sExponent = negativeExp ? (-1) * sExponent : sExponent;
 					sExponent -= decimalDigits;
-					
-					
+
 					// too large ranges
 					if (sMantissa < Constants.FLOAT_MANTISSA_MIN_RANGE
 							|| sMantissa > Constants.FLOAT_MANTISSA_MAX_RANGE
@@ -212,10 +244,9 @@ public class FloatValue extends AbstractValue {
 							|| sExponent > Constants.FLOAT_EXPONENT_MAX_RANGE) {
 						return null;
 					}
-					
-					
+
 				}
-				
+
 			}
 
 			return new FloatValue(sMantissa, sExponent);
@@ -310,7 +341,7 @@ public class FloatValue extends AbstractValue {
 				// f = mantissa * (double) (Math.pow(10, exponent));
 				long lMantissa = mantissa.longValue();
 				long lExponent = exponent.longValue();
-				
+
 				f = lMantissa * (double) (Math.pow(10, lExponent));
 			}
 		}
@@ -325,13 +356,13 @@ public class FloatValue extends AbstractValue {
 				} else if (mantissa.equals(FLOAT_POSITIVE_INFINITY)) {
 					slen = Constants.FLOAT_INFINITY_CHARARRAY.length;
 				} else {
-					assert(mantissa.equals(FLOAT_NaN));
+					assert (mantissa.equals(FLOAT_NaN));
 					slen = Constants.FLOAT_NOT_A_NUMBER_CHARARRAY.length;
 				}
 			} else {
 				// iMantissa + "E" + iExponent;
 				slenMantissa = mantissa.getCharactersLength(); // MethodsBag.getStringSize(mantissa);
-				slen = slenMantissa + 1 + exponent.getCharactersLength(); //  MethodsBag.getStringSize(exponent);
+				slen = slenMantissa + 1 + exponent.getCharactersLength(); // MethodsBag.getStringSize(exponent);
 			}
 		}
 		return slen;
@@ -339,7 +370,7 @@ public class FloatValue extends AbstractValue {
 
 	public void getCharacters(char[] cbuffer, int offset) {
 		if (exponent.equals(FLOAT_SPECIAL_VALUES)) {
-			char[] a2copy; 
+			char[] a2copy;
 			if (mantissa.equals(FLOAT_NEGATIVE_INFINITY)) {
 				// return Constants.FLOAT_MINUS_INFINITY_CHARARRAY;
 				a2copy = Constants.FLOAT_MINUS_INFINITY_CHARARRAY;
@@ -347,20 +378,20 @@ public class FloatValue extends AbstractValue {
 				// return Constants.FLOAT_INFINITY_CHARARRAY;
 				a2copy = Constants.FLOAT_INFINITY_CHARARRAY;
 			} else {
-				assert(mantissa.equals(FLOAT_NaN));
+				assert (mantissa.equals(FLOAT_NaN));
 				// return Constants.FLOAT_NOT_A_NUMBER_CHARARRAY;
 				a2copy = Constants.FLOAT_NOT_A_NUMBER_CHARARRAY;
 			}
 			System.arraycopy(a2copy, 0, cbuffer, offset, a2copy.length);
 		} else {
-			mantissa.getCharacters(cbuffer, offset); 
-			offset += + slenMantissa;
+			mantissa.getCharacters(cbuffer, offset);
+			offset += +slenMantissa;
 			cbuffer[offset++] = 'E';
-			exponent.getCharacters(cbuffer, offset); 
+			exponent.getCharacters(cbuffer, offset);
 			// return cbuffer;
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		if (exponent.equals(FLOAT_SPECIAL_VALUES)) {
@@ -369,7 +400,7 @@ public class FloatValue extends AbstractValue {
 			} else if (mantissa.equals(FLOAT_POSITIVE_INFINITY)) {
 				return Constants.FLOAT_INFINITY;
 			} else {
-				assert(mantissa.equals(FLOAT_NaN));
+				assert (mantissa.equals(FLOAT_NaN));
 				return Constants.FLOAT_NOT_A_NUMBER;
 			}
 		} else {
@@ -387,7 +418,7 @@ public class FloatValue extends AbstractValue {
 			} else if (mantissa.equals(FLOAT_POSITIVE_INFINITY)) {
 				return Constants.FLOAT_INFINITY;
 			} else {
-				assert(mantissa.equals(FLOAT_NaN));
+				assert (mantissa.equals(FLOAT_NaN));
 				return Constants.FLOAT_NOT_A_NUMBER;
 			}
 		} else {
@@ -395,34 +426,52 @@ public class FloatValue extends AbstractValue {
 		}
 	}
 
+	static long multiply(long a, long b) throws ArithmeticException {
+		long result = a * b;
+		// Note: fails for a = -1, b = Long.MIN_VALUE.
+		if (a != 0 && result / a != b) {
+			// overflow
+			throw new ArithmeticException("Overflow");
+		}
+
+		return result;
+	}
+
 	protected final boolean _equals(FloatValue o) {
 		// e.g. 10E-1 vs. 1000E-3
 		if (mantissa == o.mantissa && exponent == o.exponent) {
 			return true;
 		} else {
-			if (this.mantissa.equals(o.mantissa) && this.exponent.equals(o.exponent)){
+			if (this.mantissa.equals(o.mantissa)
+					&& this.exponent.equals(o.exponent)) {
 				return true;
 			} else {
 				long tExponent = exponent.longValue();
 				long oExponent = o.exponent.longValue();
 				long tMantissa = mantissa.longValue();
 				long oMantissa = o.mantissa.longValue();
-				
-				if (tExponent > oExponent) {
-					// e.g. 234E2 vs. 2340E1
-					long diff = tExponent - oExponent;
-					for (int i = 0; i < diff; i++) {
-						tMantissa *= 10;
+
+				try {
+					if (tExponent > oExponent) {
+						// e.g. 234E2 vs. 2340E1
+						long diff = tExponent - oExponent;
+						for (int i = 0; i < diff; i++) {
+							// tMantissa *= 10;
+							tMantissa = multiply(tMantissa, 10);
+						}
+					} else {
+						// e.g. 30E0 vs. 3E1
+						long diff = oExponent - tExponent;
+						for (int i = 0; i < diff; i++) {
+							// oMantissa *= 10;
+							oMantissa = multiply(oMantissa, 10);
+						}
 					}
 					return (tMantissa == oMantissa);
-				} else {
-					// e.g. 30E0 vs. 3E1
-					long diff = oExponent - tExponent;
-					for (int i = 0; i < diff; i++) {
-						oMantissa *= 10;
-					}
-					return (tMantissa == oMantissa);
-				}				
+				} catch (ArithmeticException e) {
+					// overflow
+					return false;
+				}
 			}
 
 		}
@@ -440,11 +489,12 @@ public class FloatValue extends AbstractValue {
 			return fv == null ? false : _equals(fv);
 		}
 	}
-	
+
 	@Override
 	public int hashCode() {
 		// Long hashCode
-		// return  (int)(mantissa  ^ (mantissa  >>> 32)) ^ (int)(exponent ^ (exponent >>> 32));
+		// return (int)(mantissa ^ (mantissa >>> 32)) ^ (int)(exponent ^
+		// (exponent >>> 32));
 		return mantissa.hashCode() ^ exponent.hashCode();
 	}
 
